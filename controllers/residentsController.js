9c28568373f7923a426e44cb4b6d4f010a66c034
IshaqKassam <ishaq.kassam@gmail.com>
2022-11-16 23:31:27 +0300

    edit resident api working

--- controllers/residentsController.js
+++ controllers/residentsController.js
@@ -21,3 +21,57 @@ exports.createResident = async (req, res) => {
 		})
 	}
 }
+
+exports.getAllResidents = async (req, res) => {
+	try {
+		await residentsModel.find({}, (err, residents) => {
+			if (err) {
+				console.log("error getting all residents")
+				return res.status(404).json({
+					message: "error getting all residents",
+				})
+			} else {
+				console.log("All residents retrieved successfully")
+				res.status(200).json(residents)
+			}
+		})
+	} catch (err) {}
+}
+exports.getOneResident = async (req, res) => {
+	const { id } = req.params
+	console.log(req.params)
+	try {
+		await residentsModel.findById(id, (err, resident) => {
+			if (err) {
+				console.log("error getting one residents")
+				return res.status(404).json({
+					message: "error getting all residents",
+				})
+			} else {
+				console.log("Searched resident retrieved successfully")
+				res.status(200).json(resident)
+			}
+		})
+	} catch (err) {}
+}
+exports.editResident = async (req, res) => {
+	const { id } = req.params
+	const { resident_name, resident_email, resident_house_number } = req.body
+	console.log(req.params)
+	console.log(req.body)
+	try {
+		await residentsModel.findOneAndUpdate(id, {
+			resident_name,
+			resident_email,
+			resident_house_number,
+		})
+
+		console.log("resident updated successfully")
+		res.status(200).json({ message: "successful" })
+	} catch (err) {
+		res.status(400).json({
+			message: "error",
+			error: err.message,
+		})
+	}
+}
--- routes/residentsRoute.js
+++ routes/residentsRoute.js
@@ -1,8 +1,11 @@
-const express = require( 'express' )
+const express = require("express")
 const router = express.Router()
 
-const residentController = require( "../controllers/residentsController.js" )
+const residentController = require("../controllers/residentsController.js")
 
-router.post( '/', residentController.createResident )
+router.post("/", residentController.createResident)
+router.get("/", residentController.getAllResidents)
+router.get("/:id", residentController.getOneResident)
+router.put("/:id", residentController.editResident)
 
 module.exports = router
